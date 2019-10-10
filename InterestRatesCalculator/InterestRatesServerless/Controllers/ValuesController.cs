90e84671497d5cc90feea12c865a24ae2a3dc011
Ridhwan Romion <Ridh-BG@hotmail.co.uk>
2019-10-10 21:39:27 +0100

    project creation

---
+++ InterestRatesCalculator/InterestRatesServerless/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterestRatesServerless.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
